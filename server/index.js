9f2b47d8e527ff8d2cb319853e2228c3073d6153
Mubasil Behzad <mubasilbehzad234@gmail.com>
2025-05-13 15:01:46 +0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
     cors({
-        origin: 'http://localhost:3000',
+        origin: 'https://mubasil-portfolio.vercel.app',
         methods: ['GET', 'POST', 'DELETE', 'PUT'],
         allowedHeaders:[
             'Content-Type',
