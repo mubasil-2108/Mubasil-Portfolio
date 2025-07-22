9c049428eba9bb4d0d580369aa5dbfc7119084a8
mubasil-2108 <mubasilbehzad234@gmail.com>
2025-07-23 00:11:05 +0500

    initial commit

--- server/index.js
+++ server/index.js
@@ -43,11 +43,11 @@ app.use(
     })
 )
 
-// app.options('*', cors());
+app.options('*', cors());
 
 
 app.use(cookieParser());
-app.use(express.json());
+// app.use(express.json());
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
